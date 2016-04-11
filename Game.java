74412b549df2dd125bf96ddec35b961100f732fc
Víctor <victorlopez94leon@gmail.com>
2016-04-11 10:31:25 +0200

    Modificado la clase Game con las localizaciones del nuevo mapa

--- Game.java
+++ Game.java
@@ -34,23 +34,25 @@ public class Game
      */
     private void createRooms()
     {
-        Room outside, theater, pub, lab, office;
+        Room terraza,salon,habitacion,pasillo,salida,cocina,despensa;
       
         // create the rooms
-        outside = new Room("outside the main entrance of the university");
-        theater = new Room("in a lecture theater");
-        pub = new Room("in the campus pub");
-        lab = new Room("in a computing lab");
-        office = new Room("in the computing admin office");
-        
+        terraza = new Room("una terraza");
+        salon = new Room("el salón de la casa");
+        habitacion = new Room("el dormitorio");
+        pasillo = new Room("el pasillo central de la casa");
+        salida = new Room("la puerta de salida");
+        cocina = new Room("la cocina");
+        despensa = new Room("una pequeña despensa llena de comida");
         // initialise room exits
-        outside.setExits(null, theater, lab, pub);
-        theater.setExits(null, null, null, outside);
-        pub.setExits(null, outside, null, null);
-        lab.setExits(outside, office, null, null);
-        office.setExits(null, null, null, lab);
-
-        currentRoom = outside;  // start game outside
+        terraza.setExits(null,salon,null,null);
+        salon.setExits(null,habitacion,pasillo,terraza);
+        habitacion.setExits(null,null,null,salon);
+        pasillo.setExits(salon,salida,cocina,null);
+        salida.setExits(null,null,null,pasillo);
+        cocina.setExits(pasillo,despensa,null,null);
+        despensa.setExits(null,null,null,cocina);
+        currentRoom = habitacion;  // start game outside
     }
 
     /**
