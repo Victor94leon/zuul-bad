88959c1d59f76abce49b126b46c0541ca2a6aa65
Víctor <victorlopez94leon@gmail.com>
2016-04-21 09:07:49 +0200

    Añadidos métodos a la clase Player para añadir,buscar y eliminar items

--- Player.java
+++ Player.java
@@ -13,18 +13,57 @@ public class Player
         playerItems = new ArrayList<Item>();
         pesoAlmacenado = 0;
     }
-    
+
     /**
      * Devuelve el peso llevado por el jugador
      */
     public int getPesoAlmacenado() {
         return pesoAlmacenado;
     }
-    
+
     /**
      * Devuelve los objetos que tiene el jugador
      */
     public ArrayList<Item> devolverItems() {
         return playerItems;
+    }
+
+    /**
+     * Añade un objeto al jugador
+     */
+    public void addItemPlayer(Item item) {
+        playerItems.add(item);
+    }
+
+    /**
+     * Busca y devuelve un objeto entre los Items del jugador que coincida con una 
+     * cadena introducida como parámetro
+     */
+    public Item buscarItemPlayer(String descripcion) {
+        int index = 0;
+        boolean encontrado = false;
+        Item item = null;
+        while(index<playerItems.size() && !encontrado) {
+            if(playerItems.get(index).getDescripcionItem().equals(descripcion)) {
+                item = playerItems.get(index);
+            }
+            index++;
+        }
+        return item;
+    }
+
+    /**
+     * Borra un objeto de los que tiene el jugador que coincida con
+     * un objeto de la clase Item introducido como parámetro
+     */
+    public void reomveItemPlayer(Item item) {
+        int index = 0;
+        boolean encontrado = false;
+        while(index<playerItems.size() && !encontrado) {
+            if(playerItems.get(index)==item) {
+                playerItems.remove(index);
+            }
+            index++;
+        }
     }
 }
